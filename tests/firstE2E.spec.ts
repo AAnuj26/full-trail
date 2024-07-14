6bf635a2cb99075990542be129fec7050b7135ab
AAnuj26 <bhagatanuj2828@gmail.com>
2024-07-14 19:33:05 +0530

    chore: Update Playwright test configuration and E2E test for fetching and posting data

--- tests/firstE2E.spec.ts
+++ tests/firstE2E.spec.ts
@@ -6,8 +6,25 @@ import { test, expect } from "@playwright/test";
 // });
 
 test.describe("Fetching And Posting Data", () => {
-  test.beforeEach(async ({ page }) => {
+  test.skip(({ browserName }) => browserName !== "chromium", "Chromium only!");
+
+  test.beforeEach(async ({ page, isMobile, context }) => {
+    test.fixme(isMobile, "Settings page does not work in mobile yet");
     await page.goto("https://proud-mud-025936b0f.5.azurestaticapps.net");
+    await context.grantPermissions(["notifications"], {
+      origin: "https://proud-mud-025936b0f.5.azurestaticapps.net",
+    });
+  });
+
+  test.describe.configure({ mode: "parallel" });
+
+  test.use({
+    viewport: { width: 1600, height: 1200 },
+    locale: "de-DE",
+    timezoneId: "Europe/Berlin",
+    geolocation: { longitude: 41.890221, latitude: 12.492348 },
+    permissions: ["geolocation"],
+    // javaScriptEnabled: false,
   });
 
   test("GetUsers Sends And Receives A Response", async ({ page }) => {
@@ -32,69 +49,48 @@ test.describe("Fetching And Posting Data", () => {
     await expect(page.locator("text=Expected Data")).toBeVisible();
   });
 
-  test("PostUsers Sends And Receives A Response", async ({ page }) => {
-    let requestSent = false;
+  test(
+    "PostUsers Sends And Receives A Response",
+    {
+      annotation: [
+        {
+          type: "issue",
+          description: "https://github.com/microsoft/playwright/issues/23180",
+        },
+        { type: "performance", description: "very slow test!" },
+      ],
+    },
+    async ({ page, browser, context }) => {
+      tag: "@report";
 
-    await page.route("**/api/v1/postData", (route) => {
-      requestSent = true;
-      // route.continue();
-      route.fulfill({
-        status: 200,
-        contentType: "application/json",
-        body: JSON.stringify({ data: "Expected Data" }),
+      await context.setGeolocation({
+        longitude: 48.858455,
+        latitude: 2.294474,
       });
-    });
+      test.info().annotations.push({
+        type: "browser version",
+        description: browser.version(),
+      });
 
-    await page.getByRole("button", { name: "PostUser" }).click();
+      let requestSent = false;
 
-    await page.waitForTimeout(2000);
+      await page.route("**/api/v1/postData", (route) => {
+        requestSent = true;
+        // route.continue();
+        route.fulfill({
+          status: 200,
+          contentType: "application/json",
+          body: JSON.stringify({ data: "Expected Data" }),
+        });
+      });
 
-    expect(requestSent).toBe(true);
+      await page.getByRole("button", { name: "PostUser" }).click();
 
-    await expect(page.locator("text=Expected Data")).toBeVisible();
-  });
+      await page.waitForTimeout(2000);
+
+      expect(requestSent).toBe(true);
+
+      await expect(page.locator("text=Expected Data")).toBeVisible();
+    }
+  );
 });
-
-// test("GetUsers Sends And Receives A Response", async ({ page }) => {
-//   let requestSent = false;
-
-//   await page.route("**/api/v1/getData", (route) => {
-//     requestSent = true;
-//     // route.continue();
-//     route.fulfill({
-//       status: 200,
-//       contentType: "application/json",
-//       body: JSON.stringify({ data: "Expected Data" }),
-//     });
-//   });
-
-//   await page.getByRole("button", { name: "GetUsers" }).click();
-
-//   await page.waitForTimeout(2000);
-
-//   expect(requestSent).toBe(true);
-
-//   await expect(page.locator("text=Expected Data")).toBeVisible();
-// });
-
-// test("PostUsers Sends And Receives A Response", async ({ page }) => {
-//   let requestSent = false;
-
-//   await page.route("**/api/v1/postData", (route) => {
-//     requestSent = true;
-//     // route.continue();
-//     route.fulfill({
-//       status: 200,
-//       contentType: "application/json",
-//       body: JSON.stringify({ data: "Expected Data" }),
-//     });
-//   });
-
-//   await page.getByRole("button", { name: "PostUser" }).click();
-
-//   await page.waitForTimeout(2000);
-
-//   expect(requestSent).toBe(true);
-
-//   await expect(page.locator("text=Expected Data")).toBeVisible();
-// });
